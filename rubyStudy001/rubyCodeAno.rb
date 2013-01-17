b36adcbcdcdfcac48f0015aa4d57ab6283b7b1b6
ChivalrousDig <ChivalrousDig@gmail.com>
2013-01-16 17:17:41 -0800

    学习ruby关于unless的用法

---
+++ rubyStudy001/rubyCodeAno.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby -w
+
+puts "Hello, Ruby!";
+
+=begin
+This is a multiline comment and con spwan as many lines as you
+like. But =begin and =end should come in the first line only. 
+=end
